9fa31ec3f7e90e9441d70da46634d5bdc2ff26da
joshuarichard <eos.josh.richard@gmail.com>
2015-08-24 01:39:49 -0400

    cuc (cleaned ugly comments)

--- test/ugh-test.py
+++ test/ugh-test.py
@@ -1,10 +1,9 @@
 import sys
 import time
 
+# example:
 # python ugh-test.py ugh 34 .05
 
-# init
-# ----
 # word - the word to be printed
 # times - the number of times you want to print the word (both down and across)
 # sleep - how long to sleep for in between lines
@@ -25,22 +24,16 @@ def main():
     # sleep so it's funnier
     time.sleep(sleep)
 
-    # now start looping - nested to going across and down
-    while (x < times):
-        while (y < times):
-
-            # add the word an extra time for each new line
+    while (x < times): # go down
+        while (y < times): # go across
             word_iter += " " + word
             print word_iter
-
-            # next iter
             y = y + 1
-            time.sleep(sleep)
-
-        # next iter    
+        time.sleep(sleep)
         x = x + 1
     
-    # now we're done going across 
+    # now we're done going across,
+    # so go down until terminated
     while(True):
         print word_iter
         time.sleep(sleep)
